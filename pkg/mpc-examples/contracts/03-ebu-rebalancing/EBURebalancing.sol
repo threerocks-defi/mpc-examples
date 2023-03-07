b9c84b90ecbf8a02f2badbdd94c7f7c32b77de0f
baileyspraggins <baileyspraggins@me.com>
2023-03-07 16:08:02 -0600

    Initial EBU Rebalancing Controller

---
+++ pkg/mpc-examples/contracts/03-ebu-rebalancing/EBURebalancing.sol
@@ -0,0 +1,108 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+pragma solidity ^0.7.0;
+pragma experimental ABIEncoderV2;
+
+// Swaps are nominally disabled
+import "@balancer-labs/v2-interfaces/contracts/pool-utils/ILastCreatedPoolFactory.sol";
+import "@balancer-labs/v2-interfaces/contracts/pool-utils/IManagedPool.sol";
+import "@balancer-labs/v2-interfaces/contracts/vault/IVault.sol";
+import "@balancer-labs/v2-solidity-utils/contracts/math/FixedPoint.sol";
+
+contract EBURebalancing {
+    using FixedPoint for uint256;
+    IVault private immutable _vault;
+    bytes32 private immutable _poolId;
+    // Assets WETH/WBTC/USDC
+    IERC20[] private _tokens;
+    IManagedPool private immutable _pool;
+    uint256 private constant _MINIMUM_DURATION_BETWEEN_REBALANCE = 30 days;
+    uint256 private constant _REBALANCE_DURATION = 7 days;
+    uint256 private _lastRebalanceCall;
+
+    constructor(IVault vault) {
+        // Get poolId from the factory
+        bytes32 poolId = IManagedPool(ILastCreatedPoolFactory(msg.sender).getLastCreatedPool()).getPoolId();
+
+        // Verify that this is a real Vault and the pool is registered - this call will revert if not.
+        vault.getPool(poolId);
+
+        // Set the global tokens variables
+        (IERC20[] memory tokens, , ) = vault.getPoolTokens(poolId);
+        _setTokens(tokens);
+
+        // Store Vault and poolId
+        _vault = vault;
+        _poolId = poolId;
+
+        _pool = _getPoolFromId(poolId);
+    }
+
+    function rebalancePool() public {
+        require(
+            block.timestamp - _lastRebalanceCall >= _MINIMUM_DURATION_BETWEEN_REBALANCE,
+            "Minimum time between calls not met"
+        );
+
+        if (isPoolPaused()) {
+            _pool.setSwapEnabled(true);
+        }
+
+        // Updates swap fee from 100% to 0.01%
+        _pool.updateSwapFeeGradually(block.timestamp, block.timestamp + _REBALANCE_DURATION, FixedPoint.ONE, 1e14);
+
+        _lastRebalanceCall = block.timestamp;
+    }
+
+    function pausePool() public {
+        require(_lastRebalanceCall + _REBALANCE_DURATION >= block.timestamp, "Pool is still rebalancing");
+        require(!isPoolPaused(), "Swaps are already paused");
+        _pool.setSwapEnabled(false);
+        // Check if the pool is rebalancing if true revert
+    }
+
+    // Get trading status function
+    // - returns boolean if swaps are paused or not
+    function isPoolPaused() public view returns (bool) {
+        return !_pool.getSwapEnabled();
+    }
+
+    function getPoolId() public view returns (bytes32) {
+        return _poolId;
+    }
+
+    function getVault() public view returns (IVault) {
+        return _vault;
+    }
+
+    function getPoolTokens() public view returns (IERC20[] memory) {
+        (IERC20[] memory tokens, , ) = _vault.getPoolTokens(_poolId);
+        return tokens;
+    }
+
+    /// === Private and Internal ===
+    function _getPoolFromId(bytes32 poolId) internal pure returns (IManagedPool) {
+        // 12 byte logical shift left to remove the nonce and specialization setting. We don't need to mask,
+        // since the logical shift already sets the upper bits to zero.
+        return IManagedPool(address(uint256(poolId) >> (12 * 8)));
+    }
+
+    function _setTokens(IERC20[] memory tokens) internal {
+        // Start index at 1 to skip BPT
+        for (uint256 i = 1; i < tokens.length; i++) {
+            _tokens.push(tokens[i]);
+        }
+    }
+}
---
+++ pkg/mpc-examples/contracts/03-ebu-rebalancing/EBURebalancingFactory.sol
@@ -0,0 +1,117 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+pragma solidity ^0.7.0;
+pragma experimental ABIEncoderV2;
+
+import "@balancer-labs/v2-interfaces/contracts/pool-utils/IManagedPool.sol";
+import "@balancer-labs/v2-interfaces/contracts/vault/IVault.sol";
+import "@balancer-labs/v2-solidity-utils/contracts/openzeppelin/Create2.sol";
+
+import "../interfaces/IManagedPoolFactory.sol";
+import "./EBURebalancing.sol";
+import "@orbcollective/shared-dependencies/contracts/TestWETH.sol";
+import "@orbcollective/shared-dependencies/contracts/TestToken.sol";
+
+/**
+ * @title EBURebalancingFactory
+ * @notice Factory for a Managed Pool and EBURebalancing Controller.
+ * @dev Determines controller deployment address, deploys pool (w/ controller address as argument), then controller.
+ */
+contract EBURebalancingFactory {
+    mapping(address => bool) public isControllerFromFactory;
+
+    address public immutable managedPoolFactory;
+    IVault public immutable balancerVault;
+    bool public isDisabled;
+
+    uint256 private _nextControllerSalt;
+    address private _lastCreatedPool;
+
+    IManagedPoolFactory.NewPoolParams private _managedPoolParams = IManagedPoolFactory.NewPoolParams({
+        name: "TestManagedPool",
+        symbol: "TMP",
+        tokens: new IERC20[](3),
+        normalizedWeights: new uint256[](3),
+        assetManagers: new address[](3),
+        swapFeePercentage: 3e15,
+        swapEnabledOnStart: true,
+        mustAllowlistLPs: false,
+        managementAumFeePercentage: 1e15,
+        aumFeeId: 0
+    });
+
+    event ControllerCreated(address indexed controller, bytes32 poolId);
+
+    constructor(IVault vault, address factory) {
+        balancerVault = vault;
+        managedPoolFactory = factory;
+
+        // Set managed pool params
+        _managedPoolParams.tokens[0] = new TestWETH(msg.sender);
+        _managedPoolParams.tokens[1] = IERC20(address(new TestToken(msg.sender, "WBTC", "WBTC", 8)));
+        _managedPoolParams.tokens[2] = IERC20(address(new TestToken(msg.sender, "USDC", "USDC", 6)));
+        _managedPoolParams.normalizedWeights[0] = 33e16;
+        _managedPoolParams.normalizedWeights[1] = 33e16;
+        _managedPoolParams.normalizedWeights[2] = 33e16;
+        _managedPoolParams.assetManagers[0] = address(0);
+        _managedPoolParams.assetManagers[1] = address(1);
+        _managedPoolParams.assetManagers[1] = address(2);
+    }
+
+    /**
+     * @dev Return the address of the most recently created pool.
+     */
+    function getLastCreatedPool() external view returns (address) {
+        return _lastCreatedPool;
+    }
+
+    function create(MinimalPoolParams memory minimalParams) external {
+        require(!isDisabled, "Factory is disabled");
+
+        bytes32 controllerSalt = bytes32(_nextControllerSalt);
+        _nextControllerSalt += 1;
+
+        bytes memory controllerCreationCode = abi.encodePacked(
+            type(EBURebalancing).creationCode,
+            abi.encode(balancerVault)
+        );
+        address expectedControllerAddress = Create2.computeAddress(controllerSalt, keccak256(controllerCreationCode));
+
+        // build arguments to deploy pool from factory
+        address[] memory assetManagers = new address[](minimalParams.tokens.length);
+        for (uint256 i = 0; i < assetManagers.length; i++) {
+            assetManagers[i] = expectedControllerAddress;
+        }
+
+        IManagedPool pool = IManagedPool(
+            IManagedPoolFactory(managedPoolFactory).create(_managedPoolParams, expectedControllerAddress)
+        );
+        _lastCreatedPool = address(pool);
+
+        address actualControllerAddress = Create2.deploy(0, controllerSalt, controllerCreationCode);
+        require(expectedControllerAddress == actualControllerAddress, "Deploy failed");
+
+        // log controller locally
+        isControllerFromFactory[actualControllerAddress] = true;
+
+        // log controller publicly
+        emit ControllerCreated(actualControllerAddress, pool.getPoolId());
+    }
+
+    // TODO: access control
+    function disable() external {
+        isDisabled = true;
+    }
+}
