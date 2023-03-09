429fa9183c9a50e1a9ac481d0012803fe932da5b
gerg <69520747+gerrrg@users.noreply.github.com>
2023-03-09 12:18:06 -0500

    Add a minimal/useless controller (NullController) so that we have common factory/test infra (#1)
    * add null controller for factory sample
    
    * Update .eslintrc.js
    
    * formatting
    
    * rm prettier from lint
    
    * lint
    
    * update yarn.lock
    
    * lint
    
    * Update pkg/mpc-examples/contracts/00-null-controller/NullControllerFactory.sol
    
    Co-authored-by: rabmarut <67169632+rabmarut@users.noreply.github.com>
    
    * Update pkg/mpc-examples/contracts/00-null-controller/NullControllerFactory.sol
    
    Co-authored-by: rabmarut <67169632+rabmarut@users.noreply.github.com>
    
    * add license header
    
    * rm vault from event
    
    * change if block to a require
    
    * create minimal pool params struct to populate downstream NewPoolParams for ManagedPool
    
    * rm arguments omitted by minimal params; add isControllerFromFactory test
    
    * lint
    
    * move from deps to devDeps
    
    * check both MPC factory and MP factory for isDisabled
    
    * add isDisabled to IMPF
    
    * lint
    
    * add inline comments explaining the arguments that are not user-provided
    
    * add Ownable access control for disabling factory
    
    * lint
    
    * add factory disable tests
    
    * Update pkg/mpc-examples/contracts/00-null-controller/NullControllerFactory.sol
    
    Co-authored-by: rabmarut <67169632+rabmarut@users.noreply.github.com>
    
    * Update pkg/mpc-examples/contracts/00-null-controller/NullControllerFactory.sol
    
    Co-authored-by: rabmarut <67169632+rabmarut@users.noreply.github.com>
    
    * Update pkg/mpc-examples/contracts/00-null-controller/NullControllerFactory.sol
    
    Co-authored-by: rabmarut <67169632+rabmarut@users.noreply.github.com>
    
    * Update pkg/mpc-examples/contracts/00-null-controller/NullControllerFactory.sol
    
    Co-authored-by: rabmarut <67169632+rabmarut@users.noreply.github.com>
    
    * Update pkg/mpc-examples/contracts/00-null-controller/NullControllerFactory.sol
    
    Co-authored-by: rabmarut <67169632+rabmarut@users.noreply.github.com>
    
    * give trader type; rm vestigial lint flag
    
    * add @dev comments
    
    * lint
    
    ---------
    
    Co-authored-by: rabmarut <67169632+rabmarut@users.noreply.github.com>

---
+++ pkg/mpc-examples/test/00-NullController.test.ts
@@ -0,0 +1,162 @@
+import { assert, expect } from 'chai';
+import { ethers } from 'hardhat';
+import { bn, fp } from '@orbcollective/shared-dependencies/numbers';
+import { Contract } from '@ethersproject/contracts';
+import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/dist/src/signer-with-address';
+import { getBalancerContractArtifact } from '@balancer-labs/v2-deployments';
+import { getBalancerContractArtifact } from '@balancer-labs/v2-deployments';
+import * as expectEvent from '@orbcollective/shared-dependencies/expectEvent';
+
+import { TokenList, setupEnvironment, pickTokenAddresses } from '@orbcollective/shared-dependencies';
+import { toNormalizedWeights } from '@balancer-labs/balancer-js';
+
+export const ZERO_ADDRESS = '0x0000000000000000000000000000000000000000';
+
+let deployer: SignerWithAddress, rando: SignerWithAddress;
+let mpcFactory: Contract;
+let tokenAddresses: string[];
+
+const initialWeights = toNormalizedWeights([fp(30), fp(70)]);
+const swapFeePercentage = bn(0.3e16);
+
+async function deployBalancerContract(
+  task: string,
+  contractName: string,
+  deployer: SignerWithAddress,
+  args: unknown[]
+): Promise<Contract> {
+  const artifact = await getBalancerContractArtifact(task, contractName);
+  const factory = new ethers.ContractFactory(artifact.abi, artifact.bytecode, deployer);
+  const contract = await factory.deploy(...args);
+  return contract;
+}
+
+async function deployController(deployer: SignerWithAddress): Promise<Contract> {
+  const newPoolParams = {
+    name: 'MyTestPool',
+    symbol: 'MTP',
+    tokens: tokenAddresses,
+    normalizedWeights: initialWeights,
+    // assetManagers: [ZERO_ADDRESS, ZERO_ADDRESS],
+    swapFeePercentage: swapFeePercentage,
+    swapEnabledOnStart: true,
+    // mustAllowlistLPs: false,
+    managementAumFeePercentage: fp(0.1),
+    aumFeeId: 0,
+  };
+
+  const receipt = await (await mpcFactory.connect(deployer).create(newPoolParams)).wait();
+  const eventController = expectEvent.inReceipt(receipt, 'ControllerCreated');
+
+  return ethers.getContractAt('NullController', eventController.args.controller);
+}
+
+/* This fn will be useful for future tests, though it is extraneous here
+async function fastForward(sec: number) {
+  const mostRecentBlock = await ethers.provider.getBlockNumber();
+  const timestamp = (await ethers.provider.getBlock(mostRecentBlock)).timestamp;
+  await ethers.provider.send('evm_mine', [timestamp + sec]);
+}
+*/
+
+async function deployBalancerManagedPoolFactory(
+  task: string,
+  libNames: string[],
+  contractName: string,
+  deployer: SignerWithAddress,
+  args: unknown[]
+): Promise<Contract> {
+  const lib_cb = await deployBalancerContract(task, libNames[0], deployer, []);
+  const lib_art = await deployBalancerContract(task, libNames[1], deployer, []);
+  const libraries = {
+    CircuitBreakerLib: lib_cb.address,
+    ManagedPoolAddRemoveTokenLib: lib_art.address,
+  };
+
+  const artifact = await getBalancerContractArtifact(task, contractName);
+  const factory = await ethers.getContractFactoryFromArtifact(artifact, { signer: deployer, libraries });
+  const instance = await factory.deploy(...args);
+  return instance.deployed();
+}
+
+async function deployLocalContract(contract: string, deployer: SignerWithAddress, args: unknown[]): Promise<Contract> {
+  const Controller = await ethers.getContractFactory(contract);
+  const c = await Controller.connect(deployer).deploy(...args);
+  const instance: Contract = await c.deployed();
+  return instance;
+}
+
+describe('NullController', function () {
+  let vault: Contract;
+  let tokens: TokenList;
+
+  before('Setup', async () => {
+    let trader: SignerWithAddress;
+    ({ vault, tokens, deployer, trader } = await setupEnvironment());
+    rando = trader;
+
+    const pfpArgs = [vault.address, fp(0.1), fp(0.1)];
+    const protocolFeesProvider = await deployBalancerContract(
+      '20220725-protocol-fee-percentages-provider/',
+      'ProtocolFeePercentagesProvider',
+      deployer,
+      pfpArgs
+    );
+
+    tokenAddresses = pickTokenAddresses(tokens, 2);
+
+    const factoryTask = 'deprecated/20221021-managed-pool';
+    const libNames = ['CircuitBreakerLib', 'ManagedPoolAddRemoveTokenLib'];
+    const factoryContract = 'ManagedPoolFactory';
+    const poolFactoryArgs = [vault.address, protocolFeesProvider.address];
+    const mpFactory = await deployBalancerManagedPoolFactory(
+      factoryTask,
+      libNames,
+      factoryContract,
+      deployer,
+      poolFactoryArgs
+    );
+
+    const controllerFactoryArgs = [vault.address, mpFactory.address];
+    mpcFactory = await deployLocalContract('NullControllerFactory', deployer, controllerFactoryArgs);
+  });
+
+  describe('Controller Deployment', () => {
+    let localController: Contract;
+    beforeEach('deploy', async () => {
+      localController = await deployController(deployer);
+    });
+
+    it("Local Controller's Vault is the Vault", async () => {
+      assert.equal(vault.address, await localController.getVault());
+    });
+
+    it('Deploys managed pool; controller set as AM for all tokens', async () => {
+      const poolId = await localController.getPoolId();
+      for (let i = 0; i < tokenAddresses.length; i++) {
+        const info = await vault.getPoolTokenInfo(poolId, tokenAddresses[i]);
+        assert.equal(info.assetManager, localController.address);
+      }
+    });
+
+    it('Controller is logged in the factory', async () => {
+      assert(await mpcFactory.isControllerFromFactory(localController.address));
+    });
+
+    it('Controller at ZERO_ADDRESS is not logged in the factory', async () => {
+      assert(!(await mpcFactory.isControllerFromFactory(ZERO_ADDRESS)));
+    });
+  });
+
+  describe('Factory Access Control', () => {
+    it('Non-owner cannot disable the factory', async () => {
+      await expect(mpcFactory.connect(rando).disable()).to.be.revertedWith('Ownable: caller is not the owner');
+    });
+
+    it('Owner can disable the factory', async () => {
+      await deployController(deployer);
+      await mpcFactory.connect(deployer).disable();
+      await expect(deployController(deployer)).to.be.revertedWith('Controller factory disabled');
+    });
+  });
+});
