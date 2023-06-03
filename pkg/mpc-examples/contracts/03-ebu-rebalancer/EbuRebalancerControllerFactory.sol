27ebede18e27dd5c60e35497eb5b26b537df32c8
baileyspraggins <baileyspraggins@me.com>
2023-06-02 20:03:38 -0500

    Rename files

--- pkg/mpc-examples/contracts/03-ebu-rebalancer/EbuRebalancerControllerFactory.sol
+++ pkg/mpc-examples/contracts/03-ebu-rebalancer/EbuRebalancerControllerFactory.sol
@@ -20,7 +20,7 @@ import "@balancer-labs/v2-interfaces/contracts/vault/IVault.sol";
 import "@balancer-labs/v2-solidity-utils/contracts/openzeppelin/Create2.sol";
 
 import "../interfaces/IManagedPoolFactory.sol";
-import "./EBURebalancerController.sol";
+import "./EbuRebalancerController.sol";
 
 import "@openzeppelin/contracts/access/Ownable.sol";
 
